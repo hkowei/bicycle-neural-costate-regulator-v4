5b586249bb52529170e5e1c709c490ba589be790
Kowei Huang <hkowei@umich.edu>
2026-05-26 17:54:27 -0400

    chores: paramter

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import numpy as np
 
 dt = 0.05 # Sampling time
 # Total time of simulation and time steps
-T_sim = 10*2
+T_sim = 15
 total_steps_sim = int(T_sim/dt)
 
 
@@ -16,7 +16,7 @@ batch_size = 100
 
 r1 = 0.5; r2 = 0.5; 
 q1 = 10.0; q2 = 25.0; q3 = 15; q4 = 10; 
-h1 = 10.0; h2 = 10.0; h3 = 10.0; h4 = 10.0
+h1 = q1*10; h2 = q2*10; h3 = q3*5; h4 = q4*5
 
 Nsample1, Nsample2, Nsample3, Nsample4 = 10, 10, 10, 10
 x_bound, y_bound, theta_bound, speed_bound = 2,2,2,2 #  sample range for initial states
@@ -35,6 +35,6 @@ u_a_max = 2
 u_omega_min = -2    # beta should be small
 u_omega_max = 2
 case = 'c'
-state_0a = np.array([[-1.16, 1.37, -1.79, 0.5]])
+state_0a = np.array([[-2, 2, -0.8, 0.1]]) # state_0a = np.array([[-1.16, 1.37, -1.79, 0.5]])
 state_0b = np.array([[-5.24, 4.11, 2.72, 0.5]])
 x_ref = 1; y_ref = 1; theta_ref = 0; speed_ref = 0
